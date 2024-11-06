adc76e0adc4c941ab37766e5d48ce5d7329775f2
tychart <tyler@thechartrands.com>
2024-11-05 20:19:22 -0700

    Changed the deploy files script

--- deployFiles.sh
+++ deployFiles.sh
@@ -14,15 +14,42 @@ if [[ -z "$host" || -z "$service" ]]; then
     exit 1
 fi
 
-printf "\n----> Deploying files for $service to $hostname with $key\n"
+#printf "\n----> Deploying files for $service to $hostname with $key\n"
 
 # Step 1
-printf "\n----> Clear out the previous distribution on the target.\n"
+#printf "\n----> Clear out the previous distribution on the target.\n"
+#ssh $host << ENDSSH
+#rm -rf services/${service}/public
+#mkdir -p services/${service}/public
+#ENDSSH
+
+# Step 2
+#printf "\n----> Copy the distribution package to the target.\n"
+#scp -r * $host:services/$service/public
+
+#---------------------------------------------------------------------------------------------------------
+printf "\n----> Deploying React bundle $service to $hostname with $key\n"
+
+# Step 1
+printf "\n----> Build the distribution package\n"
+rm -rf build
+mkdir build
+npm install # make sure vite is installed so that we can bundle
+npm run build # build the React front end
+cp -rf dist/* build # move the React front end to the target distribution
+
+# Step 2
+printf "\n----> Clearing out previous distribution on the target\n"
 ssh $host << ENDSSH
 rm -rf services/${service}/public
 mkdir -p services/${service}/public
 ENDSSH
 
-# Step 2
-printf "\n----> Copy the distribution package to the target.\n"
-scp -r * $host:services/$service/public
+# Step 3
+printf "\n----> Copy the distribution package to the target\n"
+scp -r build/* $host:services/$service/public
+
+# Step 5
+printf "\n----> Removing local copy of the distribution package\n"
+rm -rf build
+
