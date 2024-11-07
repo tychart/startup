c98ef7f50501dfe9ef18fbb7131346ebe5e11c30
tychart <tyler@thechartrands.com>
2024-11-07 16:09:23 -0700

    Adjusted the about section

--- src/about/about.jsx
+++ src/about/about.jsx
@@ -15,21 +15,15 @@ export function About(props) {
 
   return (
     <main className='container-fluid bg-secondary text-center'>
-      <div>
+      <div id="container-box">
         <div id='picture' className='picture-box'>
-          <img src={imageUrl} alt='random image' />
+          <img src="/assets/favicon.png" alt='random image' />
         </div>
 
-        <p>Simon is a repetitive memory game where you follow the demonstrated color sequence until you make a mistake. The longer the sequence you repeat, the greater your score.</p>
-
-        <p>
-          The name Simon is a registered trademark of Milton-Bradley. Our use of the name and the game is for non-profit educational use only. No part of this code or application may be used outside
-          of that definition.
-        </p>
+        <p>Tetris Dual is a spin on the classic game of Tetris in which you face off head to head on who can last the longest. </p>
 
         <div className='quote-box bg-light text-dark'>
-          <p className='quote'>{quote}</p>
-          <p className='author'>{quoteAuthor}</p>
+          <p className='quote'>This game was created by Tyler Chartrand. Any questions can be forwarded to him at tychart@byu.edu</p>
         </div>
       </div>
     </main>
